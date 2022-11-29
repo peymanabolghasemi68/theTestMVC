f302e3e288c2b14c2e99c72051b8c3daaefa9e6d
peymanabolghasemi68 <119416725+peymanabolghasemi68@users.noreply.github.com>
2022-11-29 15:34:47 +0330

    Add files via upload

---
+++ dbSchoolContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using newCoreTraining.Mapping;
+using newCoreTraining.Models;
+namespace newCoreTraining
+{
+    public class dbSchoolContext:DbContext
+    {
+        public DbSet<StudentModel> dbSetStudents { get; set; }
+        public DbSet<CourseModel> dbSetCourses { get; set; }
+        public dbSchoolContext(DbContextOptions<dbSchoolContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StudentMapping());
+            modelBuilder.ApplyConfiguration(new CourseMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
